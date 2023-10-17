cebbbd507cbb135782afedae5237a404c7c30c2e
Dinesh <dineshkore15@gmail.com>
2023-10-17 15:10:22 +0200

    updated today

--- first.py
+++ first.py
@@ -20,6 +20,22 @@ else:
 
 
 
+opt=ChromeOptions()
+opt.add_experimental_option('detach',True)
+#opt.add_argument('--headless')
+driver=webdriver.Chrome(options=opt)
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+# driver.find_element(By.XPATH,"//input[@placeholder='Username']").send_keys("Admin")
+driver.find_element(By.XPATH,"//input[@placeholder='Password']").send_keys("admin123")
+driver.find_element(By.XPATH,"//button[normalize-space()='Login']").click()
+actual_title=driver.title
+exp_title="OrangeHRM"
+
+if actual_title==exp-title:
+    print(login Test passed)
+else:
+    print("failed")
+
 
 
 
